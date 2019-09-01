99a35db456a3112f01e99a914a9881cbcefab5f5
Rodion <rodion77777@gmail.com>
2019-09-01 07:47:00 +0300

    Initial commit

---
+++ src/test/java/SBTest.java
@@ -0,0 +1,53 @@
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class SBTest {
+
+    private WebDriver driver;
+    private String baseUrl;
+
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "D:/Desktop/WebDrivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        baseUrl = "http://www.sberbank.ru/ru/person/";
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testRegion() throws Exception {
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        driver.get(baseUrl);
+
+        driver.findElement(By.xpath("//div[contains(@class,'paste-region__region header__region header__region_77')]//div//div//span")).click();
+        driver.findElement(By.xpath("//input[contains(@placeholder,'Москва')]")).sendKeys("Нижегородская область\n");
+
+        assertEquals("Ожидаемая область отличается от фактической", "Нижегородская область", driver.findElement(By.xpath("//div[@class='paste-region__region header__region header__region_52']//div//div//span")).getText());
+        driver.findElement(By.tagName("body")).sendKeys(Keys.END);
+        driver.findElement(By.xpath("//ul[@class='footer__social']")).isDisplayed();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    private void fillField(By locator, String value){
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(value);
+
+    }
+}
